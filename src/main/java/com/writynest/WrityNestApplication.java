45cb6f1634a97d91537b40cb10b262775d2db856
Kalinin Alexey <leshakalinin93@gmail.com>
2023-08-21 08:48:51 +0300

    initial commit

---
+++ src/main/java/com/writynest/WrityNestApplication.java
@@ -0,0 +1,13 @@
+package com.writynest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrityNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WrityNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/writynest/WrityNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writynest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrityNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
